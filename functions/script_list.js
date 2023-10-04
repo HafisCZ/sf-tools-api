1af5fdcc5b41979c43deb0d16d82e2eb6e9fa077
Martin <HafisCZ@users.noreply.github.com>
2023-10-04 15:18:26 +0100

    Added option to list extra scripts

--- functions/script_list.js
+++ functions/script_list.js
@@ -2,10 +2,12 @@ import { Script, wrap, respond, pickFields } from '../lib/shared'
 
 export async function handler (event, context) {
   return await wrap(context, async () => {
-    const scripts = await Script.find({ private: { $ne: true } }, 'key author created_at updated_at name description version uses');
+    const includeKeys = event.queryStringParameters.include?.split(',')
+
+    const scripts = await Script.find({ $or: [{ private: { $ne: true } }, { key: { $in: includeKeys } }] }, 'key author created_at updated_at name description version private');
 
     return respond({
-      scripts: scripts.map((script) => pickFields(script, ['author', 'key', 'created_at', 'updated_at', 'name',  'description', 'version', 'uses']))
+      scripts: scripts.map((script) => pickFields(script, ['author', 'key', 'created_at', 'updated_at', 'name',  'description', 'version', 'private']))
     })
   })
 }
